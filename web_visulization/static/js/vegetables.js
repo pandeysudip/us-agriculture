8f8e10cab94d99d3344cf146fda340ab40e20159
pandeysudip <sudip@siu.edu>
2021-11-22 23:22:00 -0800

    updating maps

--- web_visulization/static/js/vegetables.js
+++ web_visulization/static/js/vegetables.js
@@ -1,13 +1,61 @@
-// Creating our initial map object:
-// We set the longitude, latitude, and starting zoom level.
-// This gets inserted into the div with an id of "map".
-var myMap = L.map("map", {
-    center: [37.09, -95.71],
-    zoom: 5
+// Create the tile layer that will be the background of our map.
+var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+// water color 
+var watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    subdomains: 'abcd',
+    minZoom: 1,
+    maxZoom: 16,
+    ext: 'jpg'
 });
 
-// Adding a tile layer (the background map image) to our map:
-// We use the addTo() method to add objects to our map.
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// dark map 
+var dark = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 19
+});
+
+// google street 
+googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+});
+
+//google satellite
+googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+});
+
+// Create a baseMaps object to hold the streetmap layer.
+var baseMaps = {
+    "Street Map0": streetmap,
+    "Street Map1": streetmap,
+    "Street Map": streetmap,
+    "Water Color": watercolor,
+    "Dark": dark,
+    "Google Street": googleStreets,
+    "Google Sat": googleSat
+};
+//marker
+var singleMarker = L.marker([37.09, -95.71], { draggable: true });
+
+// Create an overlay object to hold our overlay.
+var overlayMaps = {
+    "First Marker": singleMarker
+};
+
+// Create our map, giving it the streetmap and earthquakes layers to display on load.
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5,
+    layers: [googleStreets, singleMarker]
+});
+
+// Add the layer control to the map.
+L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
 }).addTo(myMap);
