2531674e82e9f06c13695aeb00294a4a1c55c075
pandeysudip <sudip@siu.edu>
2021-11-22 22:30:21 -0800

    updating maps

--- web_visulization/static/js/weather.js
+++ web_visulization/static/js/weather.js
@@ -1,131 +1,62 @@
-// Store our API endpoint as queryUrl.
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
-var tectPlate = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json"
 
-
-
-
-// Perform a GET request to the query URL/
-d3.json(queryUrl).then(function (data) {
-    // Once we get a response, send the data.features object to the createFeatures function.
-    createFeatures(data.features);
+// Create the tile layer that will be the background of our map.
+var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+// water color 
+var watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    subdomains: 'abcd',
+    minZoom: 1,
+    maxZoom: 16,
+    ext: 'jpg'
 });
 
-function createFeatures(earthquakeData) {
-    // Define a function that we want to run once for each feature in the features array.
-    // Give each feature a popup that describes the place and time of the earthquake.
-    function onEachFeature(feature, layer) {
-        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
-    }
-    function chooseColor(depth) {
-        if (depth >= 500) return "red";
-        else if (depth >= 100) return "purple";
-        else if (depth >= 50) return "green";
-        else if (depth >= 20) return "black";
-        else if (depth >= 10) return "yellow";
-        else if (depth >= 5) return "blue";
-        else return "white";
-    }
-    function chooseSize(mag) {
-        if (mag > 8) return 25;
-        else if (mag > 6) return 18;
-        else if (mag > 5) return 15;
-        else if (mag > 4) return 8;
-        else if (mag > 3) return 5;
-        else return 4;
-    }
-    // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-    // Run the onEachFeature function once for each piece of data in the array.
-    var earthquakes = L.geoJSON(earthquakeData, {
-        pointToLayer: function (feature, latlng) {
-            return L.circleMarker(latlng, {
-                color: "#000",
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.8
-            });
-        },
-        style: function (feature) {
-            return {
-                fillColor: chooseColor(feature.geometry.coordinates[2]),
-                radius: chooseSize(feature.properties.mag)
-            };
-        },
-        onEachFeature: onEachFeature,
-    });
-    // Send our earthquakes layer to the createMap function/
-    createMap(earthquakes);
-}
+// dark map 
+var dark = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 19
+});
 
-function createMap(earthquakes) {
+// google street 
+googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+});
 
-    // Create the tile layer that will be the background of our map.
-    var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
-    // water color 
-    var watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        subdomains: 'abcd',
-        minZoom: 1,
-        maxZoom: 16,
-        ext: 'jpg'
-    });
+//google satellite
+googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+});
 
-    // dark map 
-    var dark = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        subdomains: 'abcd',
-        maxZoom: 19
-    });
+// Create a baseMaps object to hold the streetmap layer.
+var baseMaps = {
+    "Street Map": streetmap,
+    "Water Color": watercolor,
+    "Dark": dark,
+    "Google Street": googleStreets,
+    "Google Sat": googleSat
+};
 
-    // google street 
-    googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-        maxZoom: 20,
-        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-    });
+// Create an overlay object to hold our overlay.
 
-    //google satellite
-    googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
-        maxZoom: 20,
-        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-    });
 
-    // Create a baseMaps object to hold the streetmap layer.
-    var baseMaps = {
-        "Street Map": streetmap,
-        "Water Color": watercolor,
-        "Dark": dark,
-        "Google Street": googleStreets,
-        "Google Sat": googleSat
-    };
+// Create our map, giving it the streetmap and earthquakes layers to display on load.
+var myMap = L.map("map", {
+    center: [
+        37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [googleStreets]
+});
 
-    // Create an overlay object to hold our overlay.
 
-    var overlayMaps = {
-        Earthquakes: earthquakes,
-        "Tect Plates": d3.json(tectPlate).then(function (data) {
-            return L.geoJson(data)
-        })
-
-    };
-
-    // Create our map, giving it the streetmap and earthquakes layers to display on load.
-    var myMap = L.map("map", {
-        center: [
-            37.09, -95.71
-        ],
-        zoom: 2,
-        layers: [googleStreets, earthquakes]
-    });
-    colors = ['red', 'purple', 'green', 'black', 'yellow', 'white']
-    //myMap.addColorbar(colors = colors);
-
-    // Create a layer control.
-    // Pass it our baseMaps and overlayMaps.
-    // Add the layer control to the map.
-    L.control.layers(baseMaps, overlayMaps, {
-        collapsed: false
-    }).addTo(myMap);
-}
+// Create a layer control.
+// Pass it our baseMaps and overlayMaps.
+// Add the layer control to the map.
+L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+}).addTo(myMap);
 
