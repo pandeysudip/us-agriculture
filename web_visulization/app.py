768e7ea90e7eabbc2f7544e727c7dae63fd6a0a2
pandeysudip <sudip@siu.edu>
2021-11-24 21:49:44 -0800

    using mongoDB for data

--- web_visulization/app.py
+++ web_visulization/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect
 from pymongo import MongoClient
-import news
+import ag_news
 
 # Create an instance of Flask
 app = Flask(__name__)
@@ -10,30 +10,134 @@ app = Flask(__name__)
 client = MongoClient("mongodb://localhost:27017")
 db = client['us-agriculture']
 # creating collection
-News = db['news']
-Cropslist = db['cropslist']
+field_crops = db['field_crops']
+vegetables = db['vegetables']
+fruits = db['fruits']
+weather = db['weather']
+news = db['news']
 
 
-@app.route("/")
-def home():
+@app.route('/index.html')
+def index():
+    # Store the entire collection as a list
+    crops_list = list(field_crops.find())
+    vegetable_list = list(vegetables.find())
+    fruits_list = list(fruits.find())
+    weather_list = list(weather.find())
+    news_list = list(news.find())
 
+    # Return the template
+    return render_template('index.html', crops_list=crops_list, vegetable_list=vegetable_list, fruits_list=fruits_list, weather_list=weather_list, news_list=news_list)
+
+
+@app.route('/dashboard.html')
+def dashboard_index():
+    # Store the entire collection as a list
+    crops_list = list(field_crops.find())
+    vegetable_list = list(vegetables.find())
+    fruits_list = list(fruits.find())
+    weather_list = list(weather.find())
+    news_list = list(news.find())
+
+    # Return the template
+    return render_template('dashboard.html', crops_list=crops_list, vegetable_list=vegetable_list, fruits_list=fruits_list, weather_list=weather_list, news_list=news_list)
+
+
+@app.route('/plots.html')
+def plots_index():
+    # Store the entire collection as a list
+    crops_list = list(field_crops.find())
+    vegetable_list = list(vegetables.find())
+    fruits_list = list(fruits.find())
+    weather_list = list(weather.find())
+    news_list = list(news.find())
+
+    # Return the template
+    return render_template('plots.html', crops_list=crops_list, vegetable_list=vegetable_list, fruits_list=fruits_list, weather_list=weather_list, news_list=news_list)
+
+
+@app.route('/crops_map.html')
+def field_crops_index():
+    # Store the entire collection as a list
+    crops_list = list(field_crops.find())
+    vegetable_list = list(vegetables.find())
+    fruits_list = list(fruits.find())
+    weather_list = list(weather.find())
+    news_list = list(news.find())
+
+    # Return the template with the teams list passed in
+    return render_template('crops_map.html', crops_list=crops_list, vegetable_list=vegetable_list, fruits_list=fruits_list, weather_list=weather_list, news_list=news_list)
+
+
+@app.route('/fruits_map.html')
+def fruits_index():
+    # Store the entire collection as a list
+    crops_list = list(field_crops.find())
+    vegetable_list = list(vegetables.find())
+    fruits_list = list(fruits.find())
+    weather_list = list(weather.find())
+    news_list = list(news.find())
+
+    # Return the template with the teams list passed in
+    return render_template('fruits_map.html', crops_list=crops_list, vegetable_list=vegetable_list, fruits_list=fruits_list, weather_list=weather_list, news_list=news_list)
+
+
+@app.route('/vegetables_map.html')
+def vegetables_index():
+    # Store the entire collection as a list
+    crops_list = list(field_crops.find())
+    vegetable_list = list(vegetables.find())
+    fruits_list = list(fruits.find())
+    weather_list = list(weather.find())
+    news_list = list(news.find())
+
+    # Return the template with the teams list passed in
+    return render_template('vegetables_map.html', crops_list=crops_list, vegetable_list=vegetable_list, fruits_list=fruits_list, weather_list=weather_list, news_list=news_list)
+
+
+@app.route('/weather.html')
+def weather_index():
+    # Store the entire collection as a list
+    crops_list = list(field_crops.find())
+    vegetable_list = list(vegetables.find())
+    fruits_list = list(fruits.find())
+    weather_list = list(weather.find())
+    news_list = list(news.find())
+
+    # Return the template with the teams list passed in
+    return render_template('weather.html', crops_list=crops_list, vegetable_list=vegetable_list, fruits_list=fruits_list, weather_list=weather_list, news_list=news_list)
+
+
+@app.route('/data.html')
+def data_index():
+    # Store the entire collection as a list
+    crops_list = list(field_crops.find())
+    vegetable_list = list(vegetables.find())
+    fruits_list = list(fruits.find())
+    weather_list = list(weather.find())
+    news_list = list(news.find())
+
+    # Return the template with the teams list passed in
+    return render_template('data.html', crops_list=crops_list, vegetable_list=vegetable_list, fruits_list=fruits_list, weather_list=weather_list, news_list=news_list)
+
+
+@app.route('/news.html')
+def news_index():
     # Find one record of data from the mongo database
-    News_data = News.find_one()
-    Cropslist_data = Cropslist.find()
+    news_list = news.find_one()
 
-    # Return template and data
-    return render_template("index.html", mission_news=News_data, mission_cropslist=Cropslist_data)
-
+    # Return the template with the teams list passed in
+    return render_template('news.html', mission_news=news_list)
 
 # Route that will trigger the scrape function
-@app.route("/scrape/<name>")
-def scrape(name):
 
+
+@app.route("/scrape")
+def scrape():
     # Run the scrape function
-    news_datas = news.scraper(name)
+    news_datas = ag_news.scrape()
     # Update the Mongo database using update and upsert=True
-    News.update({}, news_datas, upsert=True)
-    # marscollection.insert_one(item)
+    news.update({}, news_datas, upsert=True)
     # Redirect back to home page
     return redirect("/")
 
